57331243c28fca6bef11e30ec729382c16a2ca31
agoelsfflake <125858022+agoelsfflake@users.noreply.github.com>
2023-02-23 01:50:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
 import streamlit
-
-streamlit.title('👌 My Parents New healthy Diner')
-streamlit.header('🫐 Breakfast Menu')
-streamlit.text('🥬 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
